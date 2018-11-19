a7504262949c5ca610cccd696f592e635ed23e7c
joseph-allen <joseph.allen.1994@hotmail.com>
2018-11-19 18:28:25 +0000

    Rename

--- src/components/header.js
+++ src/components/header.js
@@ -23,7 +23,7 @@ const Header = ({ siteTitle }) => (
             textDecoration: 'none',
           }}
         >
-          {siteTitle}
+          PyDataMCR
         </Link>
       </h1>
     </div>
